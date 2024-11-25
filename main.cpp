b7313a56a2fbf5f5ad68dde888f202b5b12a102c
ivanovichi123 <162762258+ivanovichi123@users.noreply.github.com>
2024-11-25 17:40:41 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -26,18 +26,18 @@ int main() {
         // Validar que la entrada sea un numero entero valido
         while (std::cin.fail()) {
             std::cout << "Entrada invalida. Ingrese un numero entero: ";
-			// Elimina el estado de error de std::cin
+	    // Elimina el estado de error de std::cin
             std::cin.clear();       
-			// Elimina cualquier caracter restante (maximo 1000)			
+	    // Elimina cualquier caracter restante (maximo 1000)			
             std::cin.ignore(1000, '\n');   
-			// Solicitar nuevamente al usuario que ingrese lo que se lel pide.
+	    // Solicitar nuevamente al usuario que ingrese lo que se lel pide.
             std::cin >> opcion;  
         }
 
         // Evaluar la opción seleccionada
         switch (opcion) {
             case 1:  // Limpiar el inventario
-				// Elimina todos los productos del inventario
+		// Elimina todos los productos del inventario
                 inventario.limpiar_arreglos();  
                 std::cout << "Inventario limpiado exitosamente." << std::endl;
                 break; // Termina el caso y sale del bloque switch
@@ -53,36 +53,36 @@ int main() {
                 std::cin >> antiguedad;
                 while (std::cin.fail()) {
                     std::cout <<"Entrada invalida. Ingrese un numero entero: ";
-					// Elimina el estado de error de std::cin
-					std::cin.clear();           
-					// Elimina cualquier caracter restante (maximo 1000)
-					std::cin.ignore(1000, '\n');  
-					// Solicitar nuevamente al usuario que ingrese lo que se lel pide.					
-					std::cin >> opcion;  
+		    // Elimina el estado de error de std::cin
+		    std::cin.clear();           
+		    // Elimina cualquier caracter restante (maximo 1000)
+		    std::cin.ignore(1000, '\n');  
+		    // Solicitar nuevamente al usuario que ingrese lo que se lel pide.					
+		    std::cin >> opcion;  
                 }
 
                 std::cout << "Ingrese el modelo: ";
-				// Elimina el salto de línea ('\n') dejado por std::cin
+		// Elimina el salto de línea ('\n') dejado por std::cin
                 std::cin.ignore();  
-				// Lee la línea de texto ingresada por el usuario y la almacena en 'modelo'
+		// Lee la línea de texto ingresada por el usuario y la almacena en 'modelo'
                 std::getline(std::cin, modelo); 
 
                 std::cout << "Ingrese el precio: ";
                 std::cin >> precio;
                 while (std::cin.fail()) {
                     std::cout <<"Entrada invalida. Ingrese un numero valido: ";
-					// Elimina el estado de error de std::cin
-					std::cin.clear();          
-					// Elimina cualquier caracter restante (maximo 1000)
-					std::cin.ignore(1000, '\n');  
-					// Solicitar nuevamente al usuario que ingrese lo que se lel pide.
-					std::cin >> opcion;  
+		     // Elimina el estado de error de std::cin
+		     std::cin.clear();          
+		     // Elimina cualquier caracter restante (maximo 1000)
+		     std::cin.ignore(1000, '\n');  
+		     // Solicitar nuevamente al usuario que ingrese lo que se lel pide.
+		     std::cin >> opcion;  
                 }
 
                 std::cout << "Ingrese el tipo de colchon: ";
-				// Elimina el salto de línea ('\n') dejado por std::cin
+		// Elimina el salto de línea ('\n') dejado por std::cin
                 std::cin.ignore(); 
-				// Lee la línea de texto ingresada por el usuario y la almacena en 'colchon'
+		// Lee la línea de texto ingresada por el usuario y la almacena en 'colchon'
                 std::getline(std::cin, colchon); 
 
                 // Crear y agregar la cama al inventario
@@ -102,41 +102,41 @@ int main() {
                 std::cin >> antiguedad;
                 while (std::cin.fail()) {
                     std::cout <<"Entrada invalida. Ingrese un numero entero: ";
-					// Elimina el estado de error de std::cin
-					std::cin.clear();        
-					// Elimina cualquier caracter restante (maximo 1000)
-					std::cin.ignore(1000, '\n');  
-					// Solicitar nuevamente al usuario que ingrese lo que se lel pide.
-					std::cin >> opcion;  
+		    // Elimina el estado de error de std::cin
+		    std::cin.clear();        
+		    // Elimina cualquier caracter restante (maximo 1000)
+		    std::cin.ignore(1000, '\n');  
+		    // Solicitar nuevamente al usuario que ingrese lo que se lel pide.
+		    std::cin >> opcion;  
                 }
 
                 std::cout << "Ingrese el modelo: ";
                 std::cin.ignore();
-				// Lee la línea de texto ingresada por el usuario y la almacena en 'modelo'
+		// Lee la línea de texto ingresada por el usuario y la almacena en 'modelo'
                 std::getline(std::cin, modelo); 
 
                 std::cout << "Ingrese el precio: ";
                 std::cin >> precio;
                 while (std::cin.fail()) {
                     std::cout <<"Entrada invalida. Ingrese un numero valido: ";
-					 // Elimina el estado de error de std::cin
-					std::cin.clear();      
-					// Elimina cualquier caracter restante (maximo 1000)
-					std::cin.ignore(1000, '\n');   
-					// Solicitar nuevamente al usuario que ingrese lo que se lel pide.
-					std::cin >> opcion;  
+		    // Elimina el estado de error de std::cin
+		    std::cin.clear();      
+		    // Elimina cualquier caracter restante (maximo 1000)
+		    std::cin.ignore(1000, '\n');   
+		    // Solicitar nuevamente al usuario que ingrese lo que se lel pide.
+		    std::cin >> opcion;  
                 }
 
                 std::cout << "Ingrese el numero de patas: ";
                 std::cin >> patas;
                 while (std::cin.fail()) {
                     std::cout <<"Entrada invalida. Ingrese un numero entero: ";
-					// Elimina el estado de error de std::cin
-					std::cin.clear();   
-					// Elimina cualquier caracter restante (maximo 1000)
-					std::cin.ignore(1000, '\n');
-					 // Solicitar nuevamente al usuario que ingrese lo que se lel pide.
-					std::cin >> opcion; 
+		    // Elimina el estado de error de std::cin
+		    std::cin.clear();   
+		    // Elimina cualquier caracter restante (maximo 1000)
+		    std::cin.ignore(1000, '\n');
+		    // Solicitar nuevamente al usuario que ingrese lo que se lel pide.
+		    std::cin >> opcion; 
                 }
 
                 // Crear y agregar la mesa al inventario
@@ -156,41 +156,41 @@ int main() {
                 std::cin >> antiguedad;
                 while (std::cin.fail()) {
                     std::cout <<"Entrada invalida. Ingrese un numero entero: ";
-					// Elimina el estado de error de std::cin
-					std::cin.clear();     
-					// Elimina cualquier caracter restante (maximo 1000)
-					std::cin.ignore(1000, '\n');  
-					// Solicitar nuevamente al usuario que ingrese lo que se lel pide.
-					std::cin >> opcion;  
+		    // Elimina el estado de error de std::cin
+		    std::cin.clear();     
+		    // Elimina cualquier caracter restante (maximo 1000)
+		    std::cin.ignore(1000, '\n');  
+		    // Solicitar nuevamente al usuario que ingrese lo que se lel pide.
+		    std::cin >> opcion;  
                 }
 
                 std::cout << "Ingrese el modelo: ";
                 std::cin.ignore();
-				// Lee la línea de texto ingresada por el usuario y la almacena en 'modelo'
+		// Lee la línea de texto ingresada por el usuario y la almacena en 'modelo'
                 std::getline(std::cin, modelo); 
 
                 std::cout << "Ingrese el precio: ";
                 std::cin >> precio;
                 while (std::cin.fail()) {
                     std::cout <<"Entrada invalida. Ingrese un numero valido: ";
-					// Elimina el estado de error de std::cin
-					std::cin.clear();        
-					// Elimina cualquier caracter restante (maximo 1000)
-					std::cin.ignore(1000, '\n');   
-					// Solicitar nuevamente al usuario que ingrese lo que se lel pide.
-					std::cin >> opcion;  
+		    // Elimina el estado de error de std::cin
+		    std::cin.clear();        
+		    // Elimina cualquier caracter restante (maximo 1000)
+		    std::cin.ignore(1000, '\n');   
+		    // Solicitar nuevamente al usuario que ingrese lo que se lel pide.
+		    std::cin >> opcion;  
                 }
 
                 std::cout << "Ingrese el tamano en pulgadas: ";
                 std::cin >> pulgadas;
                 while (std::cin.fail()) {
                     std::cout << "Entrada invalida. Ingrese un numero valido: ";
-					// Elimina el estado de error de std::cin
-					std::cin.clear();        
-					// Elimina cualquier caracter restante (maximo 1000)
-					std::cin.ignore(1000, '\n'); 
-					// Solicitar nuevamente al usuario que ingrese lo que se lel pide.
-					std::cin >> opcion;  
+		    // Elimina el estado de error de std::cin
+		    std::cin.clear();        
+		    // Elimina cualquier caracter restante (maximo 1000)
+		    std::cin.ignore(1000, '\n'); 
+		    // Solicitar nuevamente al usuario que ingrese lo que se lel pide.
+		    std::cin >> opcion;  
                 }
 
                 // Crear y agregar la televisión al inventario
@@ -200,13 +200,13 @@ int main() {
             }
 
             case 5:  // Mostrar todos los productos en el inventario
-				// Llama al método para imprimir todos los productos
+		// Llama al método para imprimir todos los productos
                 inventario.mostrar_productos();  
                 break; // Termina el caso y sale del bloque switch
 
             case 6:// Calcular y mostrar los impuestos totales de los productos
                 std::cout << "Impuestos totales: " 
-				<< inventario.impuesto_total() << std::endl;
+		<< inventario.impuesto_total() << std::endl;
                 break; // Termina el caso y sale del bloque switch
 
             case 7: {  // Mostrar los descuentos por vejez de los productos
@@ -214,31 +214,31 @@ int main() {
 
                 // Mostrar descuentos de camas
                 for (int i = 0; i < inventario.get_cposicion(); i++) {
-					// Obtiene la cama del inventario en la posición 'i'
+		    // Obtiene la cama del inventario en la posición 'i'
                     Cama cama = inventario.get_cama(i); 
                     // Muestra el descuento aplicado a la cama
                     std::cout << "Cama " << i + 1 << " (" 
-					<< cama.get_modelo() << "): $" 
-					<< cama.descuento_vejez() << std::endl;
+		    << cama.get_modelo() << "): $" 
+		    << cama.descuento_vejez() << std::endl;
                 }
 
                 // Mostrar descuentos de mesas
                 for (int i = 0; i < inventario.get_mposicion(); i++) {
-					// Obtiene la mesa del inventario en la posición 'i'
+		    // Obtiene la mesa del inventario en la posición 'i'
                     Mesa mesa = inventario.get_mesa(i); 
                     // Muestra el descuento aplicado a la mesa
                     std::cout << "Mesa " << i + 1 << " (" 
-					<< mesa.get_modelo() << "): $"
+		    << mesa.get_modelo() << "): $"
                     << mesa.descuento_vejez() << std::endl;
                 }
 
                 // Mostrar descuentos de televisores
                 for (int i = 0; i < inventario.get_tposicion(); i++) {
-					// Obtiene la televisión del inventario en la posición 'i'
+		    // Obtiene la televisión del inventario en la posición 'i'
                     Television tv = inventario.get_television(i); 
                     // Muestra el descuento aplicado a la televisión
                     std::cout << "Television " << i + 1 << " (" 
-					<< tv.get_modelo() << "): $"
+		    << tv.get_modelo() << "): $"
                     << tv.descuento_vejez() << std::endl;
                 }
                 break; // Termina el caso y sale del bloque switch
